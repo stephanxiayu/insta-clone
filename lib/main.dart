11505991e1fc551b6b350922a16bd40227f8fe1d
stephanxiayu <Stephan.Entwickler@gmail.com>
2022-02-01 23:55:31 +0100

    Changes to be committed: 	modified:   android/app/build.gradle 	new file:   android/app/google-services.json 	modified:   android/build.gradle 	modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instaclone/responsiv/mobile_screen_layout.dart';
 import 'package:instaclone/responsiv/responsiv_layout_screen.dart';
@@ -7,12 +8,23 @@ import 'package:instaclone/utilities/dimentions.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyBuZrzghaXl846p1L3L2kR3ksSny1oItO8",
+            appId: "1:78797791112:web:bad1fe35e747840a1ea43d",
+            messagingSenderId: "78797791112",
+            projectId: "insta-a166c",
+            storageBucket: "insta-a166c.appspot.com",
+            ),);
+  } else {
+    await Firebase.initializeApp();
+  }
 
-
-
-
-void main() {
   runApp(const MyApp());
 }
 
@@ -25,7 +37,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Insta-Clone',
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: mobileBackgroundColor, ),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
       home: const ResponsiveLayout(
         mobileScreenLayout: MobileScreenLayout(),
         webScreenLayout: WebScreenLayout(),
