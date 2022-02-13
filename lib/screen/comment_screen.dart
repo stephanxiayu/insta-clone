c935c36ebffedbe426efda653f8fac28caa0c888
stephanxiayu <Stephan.Entwickler@gmail.com>
2022-02-13 10:31:40 +0100

    Changes to be committed: 	new file:   lib/Widget/comment_card.dart 	modified:   lib/screen/comment_screen.dart

--- lib/screen/comment_screen.dart
+++ lib/screen/comment_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instaclone/Widget/comment_card.dart';
 import 'package:instaclone/utilities/colors.dart';
 
 class CommentScreen extends StatefulWidget {
@@ -16,6 +17,7 @@ class _CommentScreenState extends State<CommentScreen> {
         backgroundColor: mobileBackgroundColor,
         centerTitle: false,
       ),
+      body: CommentCard(),
       bottomNavigationBar: SafeArea(
           child: Container(
         height: kTextTabBarHeight,
@@ -28,7 +30,26 @@ class _CommentScreenState extends State<CommentScreen> {
             CircleAvatar(
               backgroundImage: NetworkImage(
                   "https://images.unsplash.com/photo-1554109394-7e351053be0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=463&q=80"),
+              radius: 18,
             ),
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.only(left: 16, right: 8),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'Comment Username', border: InputBorder.none),
+              ),
+            )),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: const Text(
+                  'Posten',
+                  style: TextStyle(color: blueColor),
+                ),
+              ),
+            )
           ],
         ),
       )),
